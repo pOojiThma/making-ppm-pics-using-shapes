5e2f1b508ced6dde5bdca73bb8570a2ff15dea18
rchhatwal3 <ramychha@charter.net>
2020-04-15 00:35:02 -0400

    Update Circle.cpp

--- Circle.cpp
+++ Circle.cpp
@@ -1,12 +1,4 @@
 /*
-Ramneek Chhatwal
-rchhatw
-CPSC 1020 Sec 001, F18
-rchhatw@g.clemson.edu
-PA3 - FROSTY THE SNOWMAN'S BEACH ADVENTURE
-*/
-
-
 #include "Circle.h"
 
 //This is the default constructor
